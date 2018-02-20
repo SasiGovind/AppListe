44d60a67157e549393a0b3b6e0e7e8e6ea0417ee
yersenia <yersenia.racerlyn@etu.u-pec.fr>
2018-02-20 16:34:40 +0100

    nouveau commit

---
+++ app/src/main/java/upec/projetandroid2017_2018/AccueilActivity.java
@@ -0,0 +1,72 @@
+package upec.projetandroid2017_2018;
+
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DataBase db;
+
+    EditText username, password;
+    Button connexion, inscription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DataBase(this);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+
+        connexion = (Button) findViewById(R.id.connexion);
+        inscription = (Button) findViewById(R.id.inscription);
+
+
+    }
+
+    private void addUser(){
+        boolean isInsered = db.addUser(username.getText().toString(), password.getText().toString());
+        if (isInsered) Toast.makeText(MainActivity.this, "Data Insered", Toast.LENGTH_LONG).show();
+        else Toast.makeText(MainActivity.this, "Data NOT Insered", Toast.LENGTH_LONG).show();
+    }
+
+    private void deleteUser(){
+        boolean b = db.deleteUser(username.getText().toString(), password.getText().toString());
+        if (b) Toast.makeText(MainActivity.this, "Deleted", Toast.LENGTH_LONG).show();
+        else  Toast.makeText(MainActivity.this, "NOT Deleted", Toast.LENGTH_LONG).show();
+    }
+
+    private void showUser(){
+                Cursor users = db.getAllUsers();
+                if (users.getCount()==0){
+                    showMessage("Error", "NOT FOUND");
+                    return;
+                }
+                StringBuffer info = new StringBuffer();
+                while (users.moveToNext()){
+                    info.append("Id : "+users.getString(0)+"\n");
+                    info.append("UserName : /"+users.getString(1)+"/\n");
+                    info.append("Password : /"+users.getString(2)+"/\n");
+                }
+                showMessage("usersList", info.toString());
+    }
+
+    private void showMessage(String title, String msg){
+        Log.i(title, "message: "+msg);
+        AlertDialog.Builder print = new AlertDialog.Builder(this);
+        print.setCancelable(true);
+        print.setTitle(title);
+        print.setMessage(msg);
+        print.show();
+    }
+}
--- app/src/main/java/upec/projetandroid2017_2018/ConnectedActivity.java
+++ app/src/main/java/upec/projetandroid2017_2018/ConnectedActivity.java
@@ -3,11 +3,11 @@ package upec.projetandroid2017_2018;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ConnectedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connected);
     }
 }
